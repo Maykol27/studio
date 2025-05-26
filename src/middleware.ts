239ec87faa13fc1ea8cf3f188d8d473162ec8dbc
Maykol Sicard <maykol.sicard27@gmail.com>
2025-05-26 17:53:41 +0000

    sigue estando el error solucionalo

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,62 +1,14 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
-import { i18n } from './i18n-config';
-import { match as matchLocale } from '@formatjs/intl-localematcher';
-import Negotiator from 'negotiator';
-
-function getLocale(request: NextRequest): string | undefined {
-  const negotiatorHeaders: Record<string, string> = {};
-  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
-
-  // @ts-ignore locales are readonly
-  const locales: string[] = i18n.locales;
-
-  let languages = new Negotiator({ headers: negotiatorHeaders }).languages(
-    locales
-  );
-
-  return matchLocale(languages, locales, i18n.defaultLocale);
-}
 
 export function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname;
-
-  // Evitar que el middleware se ejecute en rutas de API, archivos estáticos, etc.
-  if (
-    [
-      '/api',
-      '/_next/static',
-      '/_next/image',
-      '/favicon.ico',
-      // Agrega aquí cualquier otra ruta que deba ser ignorada
-    ].some((prefix) => pathname.startsWith(prefix))
-  ) {
-    return NextResponse.next();
-  }
-
-
-  const pathnameIsMissingLocale = i18n.locales.every(
-    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-  );
-
-  if (pathnameIsMissingLocale) {
-    const locale = getLocale(request);
-    return NextResponse.redirect(
-      new URL(
-        `/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`,
-        request.url
-      )
-    );
-  }
-
+  // Simplemente pasa la solicitud sin modificarla
   return NextResponse.next();
 }
 
-export const config = {
-  matcher: [
-    // Skip all internal paths (_next)
-    '/((?!_next|api|images|img|fonts|videos|favicon.ico|sitemap.xml|robots.txt).*)',
-    // Optional: only run on root (/) URL
-    // '/'
-  ],
-};
+// export const config = {
+//   matcher: [
+//     // Evitar que el middleware se ejecute en rutas de API, archivos estáticos, etc.
+//     '/((?!_next|api|images|img|fonts|videos|favicon.ico|sitemap.xml|robots.txt).*)',
+//   ],
+// };
